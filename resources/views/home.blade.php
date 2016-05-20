c6b8198b0360301aba686589a19d43328daf1377
vagrant <vagrant@homestead>
2016-05-20 17:12:06 +0000

    fix links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,12 +25,12 @@ body {
 	
 	<div class="col-md-6">
 		<div class="panel panel-primary">
-			<a href="agenda/login"><img src="agenda/public/fotos/agenda.png" alt="Agendar Citas"></a>
+			<a href="/login"><img src="/public/fotos/agenda.png" alt="Agendar Citas"></a>
 		</div>
 	</div>
 	<div class="col-md-6">
 		<div class="panel panel-primary">
-			<a href="agenda/doctor/login"><img src="agenda/public/fotos/doctor.png" alt="Hoja Medica"></a>
+			<a href="/doctor/login"><img src="/public/fotos/doctor.png" alt="Hoja Medica"></a>
 		</div>
 	</div>
 </div>
