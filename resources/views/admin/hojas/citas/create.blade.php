9e8c0ca2e57c8a49325dfc2af0e89b206217ac4c
vagrant <vagrant@homestead>
2016-05-25 20:40:58 +0000

    css changes

--- resources/views/admin/hojas/citas/create.blade.php
+++ resources/views/admin/hojas/citas/create.blade.php
@@ -23,11 +23,9 @@
 @else
 @foreach($pacientes as $paciente)
  <a type="button" data-toggle="collapse" data-target="#{{$paciente->slug}}">
- 		<ul>
-      <li class="alert alert-warning">
+      <li class="alert alert-warning no-bullet">
  			  {{ $paciente->nombres }} {{ $paciente->apellido_pat }} {{ $paciente->apellido_mat }} /{{ $paciente->tipo->code }}	
  		 </li>
-    </ul>
  </a>
 
   <div id="{{$paciente->slug}}" class="collapse">
