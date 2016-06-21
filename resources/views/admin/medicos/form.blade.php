8f3a847624c95e36b3c48ebc8d80752147d5ce51
rikardote <ricarmenta@gmail.com>
2016-06-21 10:05:24 -0700

    ett

--- resources/views/admin/medicos/form.blade.php
+++ resources/views/admin/medicos/form.blade.php
@@ -1,86 +1,90 @@
-<div class="form-group">
-	{!! Form::label('num_empleado', 'Num Empleado') !!}
-	
-	{!! Form::text('num_empleado', null, [
+<div class="col-md-6">
+	<div class="form-group">
+		{!! Form::label('num_empleado', 'Num Empleado') !!}
 		
-		'class' => 'form-control',
-		'placeholder' => 'Numero de empleado', 
-		'required'
-	]) !!}
-</div>
-<div class="form-group">
-	{!! Form::label('nombres', 'Nombres') !!}
-	
-	{!! Form::text('nombres', null, [
+		{!! Form::text('num_empleado', null, [
+			
+			'class' => 'form-control',
+			'placeholder' => 'Numero de empleado', 
+			'required'
+		]) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('nombres', 'Nombres') !!}
 		
-		'class' => 'form-control',
-		'placeholder' => 'Nombres', 
-		'required'
-	]) !!}
-</div>
-<div class="form-group">
-	{!! Form::label('apellido_pat', 'Apellido Paterno') !!}
-	
-	{!! Form::text('apellido_pat', null, [
+		{!! Form::text('nombres', null, [
+			
+			'class' => 'form-control',
+			'placeholder' => 'Nombres', 
+			'required'
+		]) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('apellido_pat', 'Apellido Paterno') !!}
 		
-		'class' => 'form-control',
-		'placeholder' => 'Apellido Paterno', 
-		'required'
-	]) !!}
-</div>
-<div class="form-group">
-	{!! Form::label('apellido_mat', 'Apellido Materno') !!}
-	
-	{!! Form::text('apellido_mat', null, [
+		{!! Form::text('apellido_pat', null, [
+			
+			'class' => 'form-control',
+			'placeholder' => 'Apellido Paterno', 
+			'required'
+		]) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('apellido_mat', 'Apellido Materno') !!}
 		
-		'class' => 'form-control',
-		'placeholder' => 'Apellido Materno', 
-		'required'
-	]) !!}
-</div>
-<div class="form-group">
-	{!! Form::label('cedula', 'Cedula Profesional') !!}
-	
-	{!! Form::text('cedula', null, [
+		{!! Form::text('apellido_mat', null, [
+			
+			'class' => 'form-control',
+			'placeholder' => 'Apellido Materno', 
+			'required'
+		]) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('cedula', 'Cedula Profesional') !!}
 		
-		'class' => 'form-control',
-		'placeholder' => 'Cedula Profesional', 
-		'required'
-	]) !!}
-</div>
-<div class="form-group">
-	{!! Form::label('especialidad_id', 'Especialidad') !!}
-	{!! Form::select('especialidad_id', $especialidades, null, [
-		'class' => 'form-control', 
-		'placeholder' => 'Selecciona una especialidad', 
-		'required'
-	]) !!}
-</div>
-<div class="form-group">
-	{!! Form::label('d_consulta', 'Dias de consulta') !!}
-	<div class="col-xl-12">
-	{!! Form::select('d_consulta[]', $diasConsulta, isset($diasconsulta_select) ? $diasconsulta_select:null, [
-		'class' => 'form-control select-tipo', 
-		'multiple', 
-		'required'
-	]) !!}
+		{!! Form::text('cedula', null, [
+			
+			'class' => 'form-control',
+			'placeholder' => 'Cedula Profesional', 
+			'required'
+		]) !!}
 	</div>
 </div>
-<div class="form-group">
-	{!! Form::label('horario_id', 'Horario') !!}
-	{!! Form::select('horario_id', $horarios, null, [
-		'class' => 'form-control',
-		'placeholder' => 'Selecciona un horario', 
-		'required'
-	]) !!}
-</div>
-<div class="form-group">
-	{!! Form::label('consultorio_id', 'Consultorio') !!}
-	{!! Form::select('consultorio_id', $consultorios, null, [
-		'class' => 'form-control', 
-		'placeholder' => 'Selecciona un Consultorio', 
-		'required'
-	]) !!}
+<div class="col-md-6">
+	<div class="form-group">
+		{!! Form::label('especialidad_id', 'Especialidad') !!}
+		{!! Form::select('especialidad_id', $especialidades, null, [
+			'class' => 'form-control', 
+			'placeholder' => 'Selecciona una especialidad', 
+			'required'
+		]) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('d_consulta', 'Dias de consulta') !!}
+		<div class="col-xl-12">
+		{!! Form::select('d_consulta[]', $diasConsulta, isset($diasconsulta_select) ? $diasconsulta_select:null, [
+			'class' => 'form-control select-tipo', 
+			'multiple', 
+			'required'
+		]) !!}
+		</div>
+	</div>
+	<div class="form-group">
+		{!! Form::label('horario_id', 'Horario') !!}
+		{!! Form::select('horario_id', $horarios, null, [
+			'class' => 'form-control',
+			'placeholder' => 'Selecciona un horario', 
+			'required'
+		]) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('consultorio_id', 'Consultorio') !!}
+		{!! Form::select('consultorio_id', $consultorios, null, [
+			'class' => 'form-control', 
+			'placeholder' => 'Selecciona un Consultorio', 
+			'required'
+		]) !!}
+	</div>
 </div>
 <script>
 	$('.select-tipo').chosen({
