28941c72fc465e3f17f80d7e87c5ea458e25769b
rikardote <ricarmenta@gmail.com>
2016-06-23 12:37:49 -0700

    tipo anidaos

--- resources/views/admin/pacientes/form_nuevo.blade.php
+++ resources/views/admin/pacientes/form_nuevo.blade.php
@@ -1,11 +1,10 @@
-{!! Form::open(['route' => ['admin.pacientes.store', $slug, $date], 'method' => 'POST']) !!}
+{!! Form::open(['route' => ['admin.pacientes.store', $slug, $date], 'name' => 'formulario', 'method' => 'POST']) !!}
 <div class="row">
 	<div class="col-md-6">
 		<div class="form-group">
 			{!! Form::label('rfc', 'RFC') !!}
 			
 			{!! Form::text('rfc', strtoupper($rfc), [
-				
 				'class' => 'form-control',
 				'placeholder' => 'Ingresar RFC', 
 				'required'
@@ -14,12 +13,15 @@
 			{!! Form::label('tipo', 'Tipo') !!}
 			{!! Form::select('tipo_id', $tipos, null, [
 				'class' => 'form-control',
+				'name' => 'tipo_id',
+				'OnChange' => 'cambiar()',
 				'placeholder' => 'Selecciona un tipo', 
 				'required'
 			]) !!}
 				{!! Form::label('gender', 'Sexo') !!}
 			{!!	Form::select('gender', array('F' => 'FEMENINO', 'M' => 'MASCULINO'), null, [
 				'class' => 'form-control',
+				'name' => 'gender',
 				'placeholder' => 'Selecciona el genero', 
 				'required'
 			]) !!}
@@ -36,7 +38,6 @@
 			{!! Form::label('apellido_pat', 'Apellido Paterno') !!}
 			
 			{!! Form::text('apellido_pat', null, [
-				
 				'class' => 'form-control',
 				'placeholder' => 'Apellido Paterno', 
 				'required'
@@ -45,7 +46,6 @@
 			{!! Form::label('apellido_mat', 'Apellido Materno') !!}
 			
 			{!! Form::text('apellido_mat', null, [
-				
 				'class' => 'form-control',
 				'placeholder' => 'Apellido Materno', 
 				'required'
@@ -57,7 +57,6 @@
 			{!! Form::label('foraneo_id', 'Ubicacion') !!}
 			{!!	Form::select('foraneo_id', array('1' => 'Mexicali', '2' => 'Ensenada', '3' => 'Tijuana', '4' => 'San Luis R.C', '5' => 'Tecate'), 1, [
 				'class' => 'form-control',
-				'placeholder' => 'Selecciona ubicación', 
 				'required'
 			]) !!}
 			{!! Form::label('phone', 'Telefono Movil') !!}
@@ -127,3 +126,47 @@
             });
         });
     </script>
+<script>
+	function cambiar()
+	{
+		var index=document.forms.formulario.tipo_id.selectedIndex;
+		
+		formulario.gender.length=0;
+		if(index==1) sexoM();
+		if(index==2) sexoF();
+		if(index==3) sexoF();
+		if(index==4) sexoM();
+		if(index==5) sexoM();
+		if(index==6) sexoF();
+		if(index==7) sexoM();
+		if(index==8) sexoF();
+		if(index==9) sexoF();
+		if(index==10) sexoM();
+		if(index==11) sexoM();
+		if(index==12) sexoF();
+		if(index==13) sexoM();
+		if(index==14) sexoF();
+		if(index==15) sexoV();
+		
+		
+	}
+function sexoF(){
+opcion0=new Option("FEMENINO","F","defauldSelected");
+
+document.forms.formulario.gender.options[0]=opcion0;
+
+}
+function sexoM(){
+opcion0=new Option("MASCULINO","M","defauldSelected");
+
+document.forms.formulario.gender.options[0]=opcion0;
+
+}
+function sexoV(){
+opcion0=new Option("FEMENINO","F","defauldSelected");
+opcion1=new Option("MASCULINO","M");
+document.forms.formulario.gender.options[0]=opcion0;
+document.forms.formulario.gender.options[1]=opcion1;
+
+}
+</script>
