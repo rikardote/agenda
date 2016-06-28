1811e57177c49cd2abf273b38678b1e9e0454030
rikardote <ricarmenta@gmail.com>
2016-06-27 19:07:06 -0700

    date test

--- resources/views/admin/pacientes/index.blade.php
+++ resources/views/admin/pacientes/index.blade.php
@@ -6,6 +6,7 @@
   <a data-url="{{ route('pacientes.create') }}" class="load-form-modal  panelColorGreen" data-toggle ="modal" data-target='#form-modal'>
     <span class="fa fa-plus-circle fa-2x" aria-hidden='true'></span>
   </a> 
+  {!! $pacientes->render() !!}
    <table class="table table-striped table-condensed">
     <thead>
         <th>RFC</th>
