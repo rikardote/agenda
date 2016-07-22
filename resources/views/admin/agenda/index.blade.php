1c6f7f1fb140d426a491b899990e431814cf4918
rikardote <ricarmenta@gmail.com>
2016-07-22 11:58:59 -0700

    fix

--- resources/views/admin/agenda/index.blade.php
+++ resources/views/admin/agenda/index.blade.php
@@ -7,13 +7,11 @@
 <div class="row">
 	@foreach($especialidades as $especialidad)
 		<div class="col-md-6 col-md-4">
-			<div class="w3-card-8 w3-green panel panel-primary">
-				<a href="{{ route('agenda.show', $especialidad->slug) }}">
+			<a href="{{ route('agenda.show', $especialidad->slug) }}">
+				<div class="w3-card-8 w3-green panel panel-primary">
 					<h4 class="text-white text-center">{{ $especialidad->name }}</h4>
-					
-				</a>
-				
-			</div>
+				</div>
+			</a>
 		</div>
 	@endforeach
 </div>
--- resources/views/admin/agenda/show.blade.php
+++ resources/views/admin/agenda/show.blade.php
@@ -7,13 +7,12 @@
 <div class="row">
 	@foreach($medicos as $medico)
 		<div class="col-md-6 col-md-4">
-			<div class="w3-card-12 w3-green panel panel-primary">
-				<a href="{{ route('admin.citas.show', [$medico->slug, $date]) }}">
+			<a href="{{ route('admin.citas.show', [$medico->slug, $date]) }}">
+				<div class="w3-card-12 w3-green panel panel-primary">
 						<strong><h5 class="text-white text-center">{{ $medico->apellido_pat }} {{ $medico->apellido_mat }} {{ $medico->nombres }}</h5></strong>
-
-					</a>
-					<div align="center"> {{isset($medico->consultorio->name) ? $medico->consultorio->name:null}} </div>
-			</div>
+						<div align="center"> {{isset($medico->consultorio->name) ? $medico->consultorio->name:null}} </div>
+				</div>
+			</a>
 		</div>
 	@endforeach
 </div>
