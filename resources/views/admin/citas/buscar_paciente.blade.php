0cabac0c65b7c6c86f2065e5c52c31aa03683b3f
vagrant <vagrant@homestead>
2016-08-15 22:20:35 -0700

    fix js pacientes

--- resources/views/admin/citas/buscar_paciente.blade.php
+++ resources/views/admin/citas/buscar_paciente.blade.php
@@ -17,21 +17,43 @@
 
 
 <script>
+$("#text-input").bind('paste', function(e) {
+     var self = this;
+     var paste = '';
+          setTimeout(function(e) {
+           rfc = $(self).val()
+            var rfc_letras = '';
+            var rfc_numero = '';
+            rfc = rfc.replace(/[^a-z0-9]/gi,'');
+            rfc_letras = rfc.substring(0,4);
+            rfc_numero = rfc.substring(4,10); 
+            if(rfc.length === 10 && (!/[^a-zA-Z]/.test(rfc_letras)) && (!/[^0-9]/.test(rfc_numero)) ) {
+                document.getElementById("mySubmit").disabled = false;
+                document.getElementById("text-input").value = rfc;
+
+            }else{
+               document.getElementById("mySubmit").disabled = true;
+            }
+          }, 0);
+});
+
 $('#text-input').keypress(function(e){
     var key = e.which;
-    return ((key >= 48 && key <= 57) || (key >= 65 && key <= 90) || (key >= 95 && key <= 122) || key==13);
+    return ((key >= 48 && key <= 57) || (key >= 65 && key <= 90) || (key >= 95 && key <= 122) || key == 13);
 });
 
 $("#text-input").on('input', function(evt) {
-	var input = $(this);
-	var start = input[0].selectionStart;
-	$(this).val(function (_, val) {
-	  return val.toUpperCase();
-	});
-	input[0].selectionStart = input[0].selectionEnd = start;
+  var input = $(this);
+  var start = input[0].selectionStart;
+  $(this).val(function (_, val) {
+    return val.toUpperCase();
+  });
+  input[0].selectionStart = input[0].selectionEnd = start;
 });
 
 document.getElementById("mySubmit").disabled = true;
+
+
 $('input[type="text"]').on('keyup',function() {
     var rfc = $('input[name="rfc"]').val();
     var rfc_letras = '';
@@ -47,19 +69,17 @@ $('input[type="text"]').on('keyup',function() {
        document.getElementById("mySubmit").disabled = true;
     }
 });
-/*
-function isAlphaNumeric(str) {
-	var code, i, len;
 
-	for (i = 0, len = str.length; i < len; i++) {
-	  code = str.charCodeAt(i);
-	  if (!(code > 47 && code < 58) && // numeric (0-9)
-	      !(code > 64 && code < 91) && // upper alpha (A-Z)
-	      !(code > 96 && code < 123)) { // lower alpha (a-z)
-	    return false;
-	  }
-	}
-	return true;
-};
-*/
+function isset ( strVariableName ) { 
+
+    try { 
+        eval( strVariableName );
+    } catch( err ) { 
+        if ( err instanceof ReferenceError ) 
+           return false;
+    }
+
+    return true;
+
+ } 
 </script>
